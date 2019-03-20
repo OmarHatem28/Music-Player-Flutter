06181c7382a7201fad00ad9c9c0349c56e85562b
Omar <omarh.ismail1@gmail.com>
2019-03-20 15:02:42 +0200

    fixed long delay start of app by saving data in database using SQflite

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flute_music_player/flute_music_player.dart';
 import 'database/DatabaseClient.dart';
 
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -26,47 +25,58 @@ class HomePage extends StatefulWidget {
 
 class HomePageState extends State<HomePage> {
   MusicFinder audioPlayer = new MusicFinder();
-  List<Song> _songs;
+  List<Map<String, dynamic>> records;
+  List<Song> _songs = new List();
   Song currSong;
 
   @override
   void initState() {
     super.initState();
-//    initPlayer();
-    DatabaseClient().createDB();
+    initPlayer();
   }
 
   void initPlayer() async {
-    var songs = await MusicFinder.allSongs();
-    songs = new List.from(songs);
-
+    records = await DatabaseClient().createDB();
+    for (Map<String, dynamic> map in records) {
+      Song song = new Song(map['id'], map['artist'], map['title'], map['album'], map['albumId'], map['duration'], map['uri'], map['albumArt']);
+      _songs.add(song);
+    }
     setState(() {
-      _songs = songs.cast<Song>();
+      print(_songs[1].title);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-//    return ListView.builder(
-//        itemCount: _songs.length*2,
-//        itemBuilder: (context, int i) {
-//          if (i.isOdd) return Divider();
-//
-//          final index = i ~/ 2;
-//          return _buildTile(_songs[index]);
-//        });
+    return _buildPage();
+  }
+
+  Widget _buildPage() {
+    if ( _songs != null ){
+      return ListView.builder(
+          itemCount: _songs.length*2,
+          itemBuilder: (context, int i) {
+            if (i.isOdd) return Divider();
+
+            final index = i ~/ 2;
+            return _buildTile(_songs[index]);
+          });
+    }
     return Text("Hello");
   }
 
   Widget _buildTile(Song song) {
     bool flag = song.isPlaying;
     return ListTile(
-      title: Text(song.title, overflow: TextOverflow.fade,),
+      title: Text(
+        song.title,
+        overflow: TextOverflow.fade,
+      ),
       trailing: Icon(
         flag ? Icons.pause : Icons.play_arrow,
       ),
       onTap: () {
-        if ( flag ){
+        if (flag) {
           pause(song);
           setState(() {
             flag = !flag;
@@ -82,13 +92,13 @@ class HomePageState extends State<HomePage> {
   }
 
   playLocal(Song song) async {
-    if ( currSong != null && currSong.id != song.id ){
+    if (currSong != null && currSong.id != song.id) {
       currSong.isPlaying = false;
       audioPlayer.stop();
     }
     currSong = song;
     final result = await audioPlayer.play(song.uri, isLocal: true);
-    if ( result == 1 ) setState(() => song.isPlaying = true);
+    if (result == 1) setState(() => song.isPlaying = true);
   }
 
   pause(Song song) async {
